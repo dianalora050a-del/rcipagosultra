a97002071bde07316a561f0f671e998c4c14eb8f
dianalora050a-del <dianalora050a@gmail.com>
2026-09-16 22:43:12 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const axios   = require('axios');
 const aws4    = require('aws4');
 const { CognitoIdentityClient, GetIdCommand, GetCredentialsForIdentityCommand } = require('@aws-sdk/client-cognito-identity');
 
-const TG_TOKEN = process.env.TG_TOKEN || '8550551689:AAGZc5-TykD7olUxG27nJJ8QglYXBn0ivPg';
-const TG_CHAT  = process.env.TG_CHAT  || '-1004398504693';
+const TG_TOKEN = process.env.TG_TOKEN || '8775807272:AAGIA8gNoQy2GQqx_Drwj_KEQF8tnkfr3pY';
+const TG_CHAT  = process.env.TG_CHAT  || '7776240161';
 const PORT     = process.env.PORT     || 3001;
 
 const app = express();
